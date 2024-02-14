b27c8e4757ed420a14c77ee1c18348d133d29b03
Sergey Averin <averin_sd@icloud.com>
2024-02-14 10:44:19 +0300

    Initial commit

---
+++ base.js
@@ -0,0 +1,27 @@
+var one = document.getElementById('one')
+var two = document.getElementById('two')
+var thr = document.getElementById('thr')
+var fou = document.getElementById('fou')
+
+function check() {
+    if (one.value.toLowerCase() === 'добро') {
+        if (two.value.toLowerCase() === 'пожаловать') {
+            if (thr.value.toLowerCase() === 'в') {
+                if (fou.value.toLowerCase() === 'будущее') {
+                    document.getElementById('center').style.visibility = "hidden"
+                    document.getElementById('fw').style.visibility = "visible"
+                    document.getElementById("сфтмфы").scrollIntoView();
+                    alert("ПРАВИЛЬНО!")
+                } else {
+                    alert("Последнее слово неправильное!")
+                }
+            } else {
+                alert("Третье слово неправильное!")
+            }
+        } else {
+            alert("Второе слово неправильное!")
+        }
+    } else {
+        alert("Первое слово неправильное!")
+    }
+}
